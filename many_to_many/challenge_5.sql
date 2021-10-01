322e0448cfcd081531a572c1c23f9b4a3ebc2a69
Daniel Pawlak <daniel@Daniels-iMac.local>
2021-10-01 13:07:50 -0700

    one_to_many, many_to_many

---
+++ many_to_many/challenge_5.sql
@@ -0,0 +1,11 @@
+SELECT 
+    genre,
+    ROUND(
+        AVG(rating),
+        2
+    )
+         AS avg_rating
+FROM series
+INNER JOIN reviews 
+    ON series.id = reviews.series_id
+GROUP BY genre;
